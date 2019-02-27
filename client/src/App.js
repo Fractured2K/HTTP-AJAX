5d333983926507dced0a95590774ad59f0775543
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-27 13:07:24 -0800

    Scaffolded Project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Friends from './components/Friends';
+import Navigation from './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
