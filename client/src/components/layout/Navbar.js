fb77eaaf489f2345d8308ce4430cbf2cecf52729
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-27 13:08:52 -0800

    Scaffolded Navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Button
+} from 'reactstrap';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+
+        return (
+            <Navbar color="dark" dark expand="md" >
+                <NavbarBrand href="/">React Friends</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Button color="primary">Add Friend</Button>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default Navigation
